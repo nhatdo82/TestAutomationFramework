6757978c07d740ec455e54de88f9c3af306886cc
Nhat Do <nhatdo@Nhats-MacBook-Pro.local>
2020-02-16 14:36:25 -0800

    add a new file

---
+++ TestAutomationFramework/page_objects/LoginPagecopy.java
@@ -0,0 +1,38 @@
+
+public class LoginPage {
+	
+	WebDriver driver;
+	//Adding more comment
+	
+	By userName = By.name("username");
+	By password = By.name("password");
+	By title	= By.className ("formtitle");
+	By login	 = By.name("btnLogin");
+	
+	public LoginPage(WebDriver driver)
+	{
+		this.driver = driver;
+	}
+	//Set UserName
+	public void enterUserName (String strUsername)
+	{
+		driver.findElement(userName).sendKeys(strUsername);
+	}
+	//Set password
+	public void enterPassword (String strPassword)
+	{
+		driver.findElement(password).sendKeys(strPassword);
+	}
+	//Click on Login Button
+	public void clickLogin()
+	{
+		driver.findElement(login).click();
+	}
+	//Get Page Title
+	public String getTitle()
+	{
+		return    driver.findElement(title).getText();
+	}
+}
+
+
